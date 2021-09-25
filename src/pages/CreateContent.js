2090efa7859cb2e4f93749620a2fdc562c491706
Zach Roberts <zach.ker.roberts@gmail.com>
2021-09-25 14:19:29 -0400

    stuff works

--- src/pages/CreateContent.js
+++ src/pages/CreateContent.js
@@ -7,13 +7,17 @@ import {
   uploadBytesResumable,
 } from 'firebase/storage';
 import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router';
 import { AuthContext } from '../context/AuthContext';
+import { createContent } from '../services/content';
 
 export const CreateContent = () => {
   const [fileInput, setFileInput] = useState();
   const { user } = useContext(AuthContext);
   const [fileUrl, setFileUrl] = useState();
   const [progress, setProgress] = useState();
+  const [summary, setSummary] = useState();
+  const history = useHistory();
   const fileOnChange = (e) => {
     if (e.target.files && e.target.files.length) {
       const storage = getStorage();
@@ -34,10 +38,21 @@ export const CreateContent = () => {
     }
   };
 
+  const onSubmit = () => {
+    createContent(summary, fileUrl).then(() => {
+      history.push(`/creator/${user.uid}`);
+    });
+  };
+
   return (
     <>
       <p>{progress}</p>
-      <Input type='textarea' placeholder='Summary'></Input>
+      <Input
+        type='textarea'
+        placeholder='Summary'
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      ></Input>
       <Button onClick={() => fileInput.click()}>Upload Profile</Button>
       <CardMedia component='video' image={fileUrl} />
       <input
@@ -46,6 +61,9 @@ export const CreateContent = () => {
         ref={setFileInput}
         onChange={fileOnChange}
       />
+      <Button onClick={() => onSubmit()} disabled={!fileInput}>
+        Submit
+      </Button>
     </>
   );
 };
