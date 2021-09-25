6992f7a721246e129b97d72d588deaa911c92753
Dan Boudet <dboudet04@gmail.com>
2021-09-25 15:45:43 -0400

    user content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,38 @@
-import { Card } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
 
 export const Content = (props) => {
-  console.log(props);
-  return <Card></Card>;
+  const classes = useStyles();
+  const { summary, file } = props
+
+  return (
+    <Card className={classes.card} sx={{ maxWidth: 345 }}>
+      <video width="320" height="240" controls>
+          <source src={props.file} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        {/* <CardMedia
+        component="video"
+        height="140"
+        src={props.file}
+        alt="video"
+      /> */}
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {props.summary}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
 };
--- src/pages/Creator.js
+++ src/pages/Creator.js
@@ -1,26 +1,26 @@
-import { Button, Paper, Typography } from '@material-ui/core';
-import { useContext, useEffect, useState } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import { getCreator } from '../services/creator';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-import { AuthContext } from '../context/AuthContext';
-import { makeStyles } from '@material-ui/styles';
-import { getContent } from '../services/content';
-import { Content } from '../components/Content';
+import { Button, Paper, Typography } from "@material-ui/core"
+import { useContext, useEffect, useState } from "react"
+import { useHistory, useParams } from "react-router-dom"
+import { getCreator } from "../services/creator"
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage"
+import { AuthContext } from "../context/AuthContext"
+import { makeStyles } from "@material-ui/styles"
+import { getContent } from "../services/content"
+import { Content } from "../components/Content"
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    minHeight: '80vh',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    minHeight: "80vh",
     padding: theme.spacing(2),
   },
   creatorImage: {
     height: 350,
     width: 350,
-    borderRadius: '50%',
+    borderRadius: "50%",
     margin: theme.spacing(4),
   },
   name: {
@@ -32,49 +32,54 @@ const useStyles = makeStyles((theme) => ({
   addContentButton: {
     margin: theme.spacing(4),
   },
-}));
+  contentDiv: {
+    display: "flex",
+    justifyContent: "center",
+    flexWrap: "wrap",
+  },
+}))
 
 export default function Creator() {
-  const classes = useStyles();
-  const history = useHistory();
-  const params = useParams();
-  const [creator, setCreator] = useState();
-  const [fileInput, setFileInput] = useState();
-  const [canEdit, setEdit] = useState(false);
-  const [profileImageUrl, setProfileImageUrl] = useState();
-  const [content, setContent] = useState([]);
-  const { user } = useContext(AuthContext);
+  const classes = useStyles()
+  const history = useHistory()
+  const params = useParams()
+  const [creator, setCreator] = useState()
+  const [fileInput, setFileInput] = useState()
+  const [canEdit, setEdit] = useState(false)
+  const [profileImageUrl, setProfileImageUrl] = useState()
+  const [content, setContent] = useState([])
+  const { user } = useContext(AuthContext)
   useEffect(() => {
     if (params) {
-      getCreator(params.creatorId).then(setCreator);
-      getContent(params.creatorId).then(setContent);
+      getCreator(params.creatorId).then(setCreator)
+      getContent(params.creatorId).then(setContent)
     }
-  }, [params]);
+  }, [params])
 
   useEffect(() => {
     if (user && creator) {
       if (user.uid === creator.firebaseId) {
-        setEdit(true);
+        setEdit(true)
       }
     }
-  }, [creator, user]);
+  }, [creator, user])
 
   useEffect(() => {
-    const storage = getStorage();
-    const reference = ref(storage, `profile/${params.creatorId}`);
-    getDownloadURL(reference).then(setProfileImageUrl);
-  }, []);
+    const storage = getStorage()
+    const reference = ref(storage, `profile/${params.creatorId}`)
+    getDownloadURL(reference).then(setProfileImageUrl)
+  }, [])
 
   const fileOnChange = (e) => {
     if (e.target.files && e.target.files.length) {
-      const storage = getStorage();
-      const reference = ref(storage, `profile/${params.creatorId}`);
+      const storage = getStorage()
+      const reference = ref(storage, `profile/${params.creatorId}`)
       uploadBytes(reference, e.target.files[0]).then((res) => {
-        console.log(res.ref);
-        getDownloadURL(res.ref).then(setProfileImageUrl);
-      });
+        console.log(res.ref)
+        getDownloadURL(res.ref).then(setProfileImageUrl)
+      })
     }
-  };
+  }
 
   return (
     <Paper className={classes.paper}>
@@ -84,7 +89,7 @@ export default function Creator() {
           onClick={() => canEdit && fileInput.click()}
           className={classes.creatorImage}
           style={{
-            cursor: canEdit ? 'pointer' : '',
+            cursor: canEdit ? "pointer" : "",
           }}
         />
       )}
@@ -92,32 +97,34 @@ export default function Creator() {
         <Button onClick={() => fileInput.click()}>Upload Profile</Button>
       )}
       <input
-        style={{ display: 'none' }}
-        type='file'
+        style={{ display: "none" }}
+        type="file"
         ref={setFileInput}
         onChange={fileOnChange}
       />
-      <Typography className={classes.name} variant='h4' component='h2'>
+      <Typography className={classes.name} variant="h4" component="h2">
         {creator?.name}
       </Typography>
-      <Typography className={classes.bio} variant='body1'>
+      <Typography className={classes.bio} variant="body1">
         {creator?.bio}
       </Typography>
 
       {canEdit && (
         <Button
           className={classes.addContentButton}
-          variant='contained'
-          color='primary'
-          size='large'
-          onClick={() => history.push('/create-content')}
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={() => history.push("/create-content")}
         >
           Add Content
         </Button>
       )}
-      {content?.map((content) => {
-        return <Content {...content} />;
-      })}
+      <div className={classes.contentDiv}>
+        {content?.map((content) => {
+          return <Content {...content} />
+        })}
+      </div>
     </Paper>
-  );
+  )
 }
